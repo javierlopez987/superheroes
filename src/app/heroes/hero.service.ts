8bd1ab0334a1c1c4dc0261e71cf04810611a823a
Javier Lopez Ferreira <javierlopez987@gmail.com>
2023-09-05 18:14:11 -0300

    mejora layout

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -29,6 +29,54 @@ export class HeroService {
         "power": "61",
         "combat": "100"
       }
+    ),
+    new Hero(
+      "Cloak", 
+      "https://www.superherodb.com/pictures2/portraits/10/100/46.jpg",
+      {
+        "intelligence": "63",
+        "strength": "10",
+        "speed": "47",
+        "durability": "64",
+        "power": "100",
+        "combat": "56"
+      }
+    ),
+    new Hero(
+      "Colossus", 
+      "https://www.superherodb.com/pictures2/portraits/10/100/48.jpg",
+      {
+        "intelligence": "63",
+        "strength": "83",
+        "speed": "33",
+        "durability": "100",
+        "power": "45",
+        "combat": "80"
+      }
+    ),
+    new Hero(
+      "Crimson Dynamo", 
+      "https://www.superherodb.com/pictures2/portraits/10/100/1093.jpg",
+      {
+        "intelligence": "null",
+        "strength": "65",
+        "speed": "null",
+        "durability": "null",
+        "power": "null",
+        "combat": "null"
+      }
+    ),
+    new Hero(
+      "Cyborg Superman", 
+      "https://www.superherodb.com/pictures2/portraits/10/100/667.jpg",
+      {
+        "intelligence": "75",
+        "strength": "93",
+        "speed": "92",
+        "durability": "100",
+        "power": "100",
+        "combat": "80"
+      }
     )
   ];
 
